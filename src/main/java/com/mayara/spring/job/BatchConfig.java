d210bc0830d12c3ad39199a70e7378915ad1d885
Mayara <ssmayarah@gmail.com>
2021-07-07 17:04:47 -0300

    novo job par ou impar e refatoracao do job ola mundo

--- src/main/java/com/mayara/spring/job/BatchConfig.java
+++ src/main/java/com/mayara/spring/job/BatchConfig.java
@@ -1,4 +1,4 @@
-package com.mayara.spring;
+package com.mayara.spring.job;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
@@ -6,10 +6,13 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,29 +23,12 @@ public class BatchConfig {
   @Autowired
   private JobBuilderFactory jobBuilderFactory;
 
-  @Autowired
-  private StepBuilderFactory stepBuilderFactory;
-
   @Bean
-  public Job imprimeOlaJob() {
+  public Job imprimeOlaJob(Step imprimeOlaStep) {
     return jobBuilderFactory
         .get("imprimeOlaJob")
-        .start(imprimeOlaStep())
+        .start(imprimeOlaStep)
+        .incrementer(new RunIdIncrementer())
         .build();
-  }
-
-  public Step imprimeOlaStep() {
-    return stepBuilderFactory
-        .get("imprimeOlaStep")
-        .tasklet(new Tasklet() {
-          @Override
-          public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext)
-              throws Exception {
-            System.out.printf("ola, mundo da Mayara!!");
-            return RepeatStatus.FINISHED;
-          }
-        })
-        .build();
-
   }
 }
