1de63511aea972208683d3bfed10718dbdf35f42
Mayara <ssmayarah@gmail.com>
2021-07-30 16:31:01 -0300

    adicionando processador composto

--- src/main/java/com/mayara/spring/processor/ProcessadorValidacaoProcessorConfig.java
+++ src/main/java/com/mayara/spring/processor/ProcessadorValidacaoProcessorConfig.java
@@ -4,6 +4,8 @@ import com.mayara.spring.dominio.Cliente;
 import java.util.HashSet;
 import java.util.Set;
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.support.builder.CompositeItemProcessorBuilder;
+import org.springframework.batch.item.validator.BeanValidatingItemProcessor;
 import org.springframework.batch.item.validator.ValidatingItemProcessor;
 import org.springframework.batch.item.validator.ValidationException;
 import org.springframework.batch.item.validator.Validator;
@@ -16,22 +18,35 @@ public class ProcessadorValidacaoProcessorConfig {
   private Set<String> emails = new HashSet<>();
 
   @Bean
-  public ItemProcessor<Cliente, Cliente> processadorValidacaoProcessor() {
-//    BeanValidatingItemProcessor<Cliente> processor = new BeanValidatingItemProcessor<>();
-//    processor.setFilter(true);
+  public ItemProcessor<Cliente, Cliente> processadorValidacaoProcessor() throws Exception {
+    return new CompositeItemProcessorBuilder<Cliente, Cliente>()
+        .delegates(beanValidatingItemProcessor(), emailValidationProcessor())
+        .build();
+
+  }
+
+  private BeanValidatingItemProcessor<Cliente> beanValidatingItemProcessor() throws Exception {
+    BeanValidatingItemProcessor<Cliente> processor = new BeanValidatingItemProcessor<>();
+    processor.setFilter(true);
+    processor.afterPropertiesSet();
+    return processor;
+  }
+
+  private ValidatingItemProcessor<Cliente> emailValidationProcessor() {
     ValidatingItemProcessor<Cliente> processor = new ValidatingItemProcessor<>();
     processor.setValidator(validator());
     processor.setFilter(true);
     return processor;
-
   }
 
   private Validator<Cliente> validator() {
     return new Validator<Cliente>() {
       @Override
       public void validate(Cliente cliente) throws ValidationException {
-        if(emails.contains(cliente.getEmail()))
-          throw new ValidationException(String.format("O cliente %s já foi processado!" , cliente.getEmail()));
+        if (emails.contains(cliente.getEmail())) {
+          throw new ValidationException(
+              String.format("O cliente %s já foi processado!", cliente.getEmail()));
+        }
         emails.add(cliente.getEmail());
       }
     };
