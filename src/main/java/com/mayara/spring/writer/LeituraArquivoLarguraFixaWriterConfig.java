83e3122aeecfee26f6a66150ae1a4c675ca6fe8d
Mayara <ssmayarah@gmail.com>
2021-07-30 18:03:23 -0300

    escritorFlatFormatado

--- src/main/java/com/mayara/spring/writer/LeituraArquivoLarguraFixaWriterConfig.java
+++ src/main/java/com/mayara/spring/writer/LeituraArquivoLarguraFixaWriterConfig.java
@@ -1,23 +1,28 @@
 package com.mayara.spring.writer;
 
 import com.mayara.spring.dominio.Cliente;
-import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 
 @Configuration
 public class LeituraArquivoLarguraFixaWriterConfig {
+
+  @StepScope
   @Bean
-  public ItemWriter<Cliente> leituraArquivoLarguraFixaWriter() {
-    return items -> items.forEach(System.out::println);
-//    return items -> {
-//      for(Cliente cliente : items){
-//        if (cliente.getNome().equals("Maria"))
-//          throw new Exception();
-//        else {
-//          System.out.println(cliente);
-//        }
-//      }
-//    };
+  public FlatFileItemWriter<Cliente> escritaArquivoLarguraFixaWriter(
+      @Value("#{jobParameters['arquivoClientesSaida']}")
+          Resource resource) {
+    return new FlatFileItemWriterBuilder()
+        .name("escritaArquivoLarguraFixaWriter")
+        .resource(resource)
+        .formatted()
+        .format("%-9s %-9s %-2s %-19s")
+        .names("nome", "Sobrenome", "idade", "email")
+        .build();
   }
 }
