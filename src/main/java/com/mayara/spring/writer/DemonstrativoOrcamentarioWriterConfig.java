6f7bc9fda3d1f5dcc79ec267d2b60317b0bef805
Mayara <ssmayarah@gmail.com>
2021-07-31 13:33:41 -0300

    ajuste writer

--- src/main/java/com/mayara/spring/writer/DemonstrativoOrcamentarioWriterConfig.java
+++ src/main/java/com/mayara/spring/writer/DemonstrativoOrcamentarioWriterConfig.java
@@ -8,7 +8,6 @@ import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import org.springframework.batch.core.configuration.annotation.StepScope;
-import org.springframework.batch.item.file.FlatFileFooterCallback;
 import org.springframework.batch.item.file.FlatFileHeaderCallback;
 import org.springframework.batch.item.file.FlatFileItemWriter;
 import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
@@ -20,43 +19,6 @@ import org.springframework.core.io.Resource;
 
 @Configuration
 public class DemonstrativoOrcamentarioWriterConfig {
-
-//  @Bean
-//  public ItemWriter<GrupoLancamento> demonstrativoOrcamentarioWriter() {
-//    return itens -> {
-//      System.out.println("\n");
-//      System.out.println(String.format("SISTEMA INTEGRADO: XPTO \t\t\t\t DATA: %s",
-//          new SimpleDateFormat("dd/MM/yyyy").format(new Date())));
-//      System.out.println(String.format("MÓDULO: ORÇAMENTO \t\t\t\t\t HORA: %s",
-//          new SimpleDateFormat("HH:MM").format(new Date())));
-//      System.out.println(String.format("\t\t\tDEMONSTRATIVO ORCAMENTARIO"));
-//      System.out.println(String
-//          .format("----------------------------------------------------------------------------"));
-//      System.out.println(String.format("CODIGO NOME VALOR"));
-//      System.out.println(String.format("\t Data Descricao Valor"));
-//      System.out.println(String
-//          .format("----------------------------------------------------------------------------"));
-//
-//      Double totalGeral = 0.0;
-//      for (GrupoLancamento grupo : itens) {
-//        System.out.println(String.format("[%d] %s - %s", grupo.getCodigoNaturezaDespesa(),
-//            grupo.getDescricaoNaturezaDespesa(),
-//            NumberFormat.getCurrencyInstance().format(grupo.getTotal())));
-//        totalGeral += grupo.getTotal();
-//        for (Lancamento lancamento : grupo.getLancamentos()) {
-//          System.out.println(String.format("\t [%s] %s - %s",
-//              new SimpleDateFormat("dd/MM/yyyy").format(lancamento.getData()),
-//              lancamento.getDescricao(),
-//              NumberFormat.getCurrencyInstance().format(lancamento.getValor())));
-//        }
-//      }
-//      System.out.println("\n");
-//      System.out.println(String.format("\t\t\t\t\t\t\t  Total: %s",
-//          NumberFormat.getCurrencyInstance().format(totalGeral)));
-//      System.out.println(
-//          String.format("\t\t\t\t\t\t\t  Código de Autenticação: %s", "fkyew6868fewjfhjjewf"));
-//    };
-//  }
 
   @StepScope
   @Bean
